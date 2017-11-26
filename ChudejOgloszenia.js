e1496e857a5be841625e4304f41d2c20ce7d7850
Shadoba <firebart1@gmail.com>
2017-11-26 16:26:55 +0100

    Add files via upload

---
+++ ChudejOgloszenia.js
@@ -0,0 +1,11 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+window.location.href = "https://www.gumtree.pl/post.html";
+
+var CategorySelect = document.getElementById('catSelector').children[1];
+
+alert(CategorySelect.lastChild);
